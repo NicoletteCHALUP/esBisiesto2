3f728e9ec5408d15de07005c35d93f992e7ef039
Nicolette <nicolette.chalup@ucb.edu.bo>
2024-02-25 21:54:44 -0400

    Toma en cuenta que los anios divisibles por  400 SON años bisiestos por ejemplo 2000

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,8 +1,8 @@
 function esBisiesto(anio){
-    if(anio==4){
-        return true;
+    if(anio % 400 === 0){
+      return true;
     }
     return false;
-  }
+}
 
-  export default esBisiesto;
+export default esBisiesto;
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,12 +1,16 @@
 import esBisiesto from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
-    it("El 4 deberia considerarse como anio bisiesto", () => {
-      expect(esBisiesto(4)).toEqual(true);
+    it("El 400 deberia considerarse como anio bisiesto", () => {
+      expect(esBisiesto(400)).toEqual(true);
     });
 
     it("El 5 NO deberia considerarse como anio bisiesto", () => {
-        expect(esBisiesto(5)).toEqual(false);
+        expect(esBisiesto(500)).toEqual(false);
+      });
+      
+    it("Todos los años divisibles por  400 SON años bisiestos (así, por ejemplo, 2000 fue un año bisiesto),", () => {
+        expect(esBisiesto(2000)).toEqual(true);
       });
   });
 
